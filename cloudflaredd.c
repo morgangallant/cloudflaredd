61401464c750e537a833a05688853ff25a67e471
Morgan Gallant <morgan@morgangallant.com>
2020-01-04 18:08:54 -0500

    added licensing

--- cloudflaredd.c
+++ cloudflaredd.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2020 Morgan Gallant
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include <arpa/inet.h>
 #include <assert.h>
 #include <netdb.h>
@@ -186,6 +207,9 @@ static char *stun_client_read_response(stun_client_t *client) {
 // Cleanup the stun client by closing the bound socket.
 static void stun_client_cleanup(stun_client_t *client) { close(client->fd); }
 
+// The address of the public Google-hosted stun server to use.
+#define GOOGLE_STUN_ADDR "stun.l.google.com:19302"
+
 // Get the public ip address of this server.
 static char *stun_get_pub_ip(const char *stun_server_addr) {
   stun_addr_t *addr = stun_addr_create(stun_server_addr);
@@ -205,8 +229,7 @@ done:
   return pub_ip;
 }
 
-// The address of the public Google-hosted stun server to use.
-#define GOOGLE_STUN_ADDR "stun.l.google.com:19302"
+/* Cloudflare API Implementation */
 
 int main() {
   const char *ip = stun_get_pub_ip(GOOGLE_STUN_ADDR);
